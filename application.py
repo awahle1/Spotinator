88fcaeccb4ca5fa9e5d13a86d115299d443a56a8
awahle1 <austinwahle1@gmail.com>
2021-02-01 20:53:27 -0500

    Voting is working, but kinka janky, about to make big changes

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyAuthBase
 import spotipy.util as util
 import json
+import math
 
 #znkum3uqbtji62g19resp3pwf
 
@@ -15,6 +16,10 @@ username = None
 
 read = None
 control = None
+
+votes = 0
+members = 0
+required = 0
 
 
 def make_auths(username):
@@ -29,6 +34,9 @@ def make_auths(username):
 	token = util.prompt_for_user_token(username, scope,client_id=cid,client_secret=secret,redirect_uri='http://127.0.0.1:9090')
 	control = spotipy.Spotify(auth=token)
 
+def get_required(members):
+	required = math.ceil(float(members+1)*.8)
+	return(required)
 
 
 
@@ -48,8 +56,6 @@ def join():
 @app.route("/pausePlay", methods=['post'])
 def pause_play():
 	value = request.form.get("value")
-	print("-----------------------------------")
-	print(value)
 	if value == "play":
 		control.start_playback()
 		response = {'word':'pause'}
@@ -62,10 +68,46 @@ def pause_play():
 @app.route("/session", methods=['post'])
 def session():
 	global username
+	global members
 	username = request.form.get("username")
 	make_auths(username)
 	current_track = read.current_user_playing_track()['item']['name']
+	
+	members = 1
 	return render_template("session.html", track = current_track)
+
+@app.route("/session_member")
+def session_member():
+	global members
+	global votes
+	current_track = read.current_user_playing_track()['item']['name']
+	members +=1
+	required = get_required(members)
+	return render_template("session_member.html", track = current_track)
+
+@app.route("/vote", methods=['post'])
+def vote():
+	global members
+	global votes
+	global required
+
+	required = math.ceil(float(members+1)*.8)
+	value = request.form.get("value")
+	votes += int(value)
+
+	current_track = read.current_user_playing_track()['item']['name']
+
+
+	if votes>=required:
+		control.next_track()
+		current_track = read.current_user_playing_track()['item']['name']
+		votes = 0
+		response = {'count': votes, 'members': required, 'reset' :'yes', 'track': current_track}
+	else:
+		response = {'count': votes, 'members': required, 'reset': "no", 'track': current_track}
+
+	
+	return (json.dumps(response))	
 
 
 
